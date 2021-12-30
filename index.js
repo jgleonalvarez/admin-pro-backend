7199b0e099db4070ca0d3bfc5884b1f856dbf718
Jose Gpe Leon <jose.leon@eon.com.mx>
2021-12-29 20:32:11 -0600

    SPA Control

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // console log('Hola Mundo');
 require('dotenv').config();
+
+const path = require('path');
 const express = require('express');
 const { dbConn } = require('./database/config');
 const cors = require('cors');
@@ -34,6 +36,10 @@ app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/login', require('./routes/auth'));
 app.use('/api/uploads', require('./routes/uploads'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+})
 
 app.listen(process.env.PORT, () => {
     console.log('Servidor corriendo en puerto ' + process.env.PORT);
